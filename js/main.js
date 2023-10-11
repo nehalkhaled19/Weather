6bd909ee208d03912d991b476eb92991195a4e94
nehalkhaled19 <nehalkhaled62@gmail.com>
2023-10-11 20:43:36 +0300

    sec commit

--- js/main.js
+++ js/main.js
@@ -1,92 +1,65 @@
 let todayName = document.getElementById("todayName")
-const tomorrowName = document.getElementById("tomorrowName")
-const nextDayName = document.getElementById("nextDayName")
 const month = document.getElementById("month")
 const cityName = document.getElementById("cityName")
-const l = document.getElementById("l")
-const weatherState = document.getElementById("weatherState")
 const citydegree = document.getElementById("citydegree")
+const weatherState = document.getElementById("weatherState")
 const conditionToday = document.getElementById("conditionToday")
-const maxTempTomm = document.getElementById("maxTempTomm")
-const minTempTomm = document.getElementById("minTempTomm")
-const tommState = document.getElementById("tommState")
-const tommIcon = document.getElementById("tommIcon")
-const maxTempNextday = document.getElementById("maxTempNextday")
-const minTempNextday = document.getElementById("minTempNextday")
-const nextdayState = document.getElementById("nextdayState")
-const nextdayIcon = document.getElementById("nextdayIcon")
+const humidity = document.getElementById("humidity")
 const wind = document.getElementById("wind")
-const date = new Date();
-locationCity("cairo")
-weather("cairo")
+let windDirection = document.getElementById("wind_direction")
 
-// weathertoday
-async function locationCity(city = "cairo") {
-    let myReq = await fetch(`http://api.weatherapi.com/v1/current.json?key= 3b1c938fe07a415dad0213021230308&q=${city}`)
-    let data = await myReq.json()
-    cityName.innerHTML =data.location.name
-    citydegree.innerHTML = data.current.temp_c + "°C"
-    conditionToday.innerHTML = data.current.condition.text
-    // let x =`https:${data.current.condition.icon}`
-    // weatherState.src = x
-    wind.innerHTML = data.current.wind_kph
-}
+
+//next data
+const nextDay = document.getElementsByClassName("next-day")
+const icon = document.getElementsByClassName("icon")
+const maxTemp = document.getElementsByClassName("max-temp")
+const minTemp = document.getElementsByClassName("min-temp")
+const state = document.getElementsByClassName("state")
+
+// search
+const searchByName = document.getElementById("searchByName")
+
+weatherData()
+
+
+
 //weather 
-async function weather(city = "cairo") {
-    // tomorrow
+async function weatherData(city = "cairo") {
     let myReq = await fetch(`http://api.weatherapi.com/v1/forecast.json?key= 3b1c938fe07a415dad0213021230308&q=${city}&days=3`)
     let data = await myReq.json()
-    maxTempTomm.innerHTML = data.forecast.forecastday[1].day.maxtemp_c + "°C"
-    minTempTomm.innerHTML = data.forecast.forecastday[1].day.mintemp_c + "°C"
-    tommState.innerHTML = data.forecast.forecastday[1].day.condition.text
-    let x =`https:${data.forecast.forecastday[1].day.condition.icon}`
-    tommIcon.src = x
-    // nextday
-    maxTempNextday.innerHTML = data.forecast.forecastday[2].day.maxtemp_c + "°C"
-    minTempNextday.innerHTML = data.forecast.forecastday[2].day.mintemp_c + "°C"
-    nextdayState.innerHTML = data.forecast.forecastday[2].day.condition.text
-    let y=`https:${data.forecast.forecastday[2].day.condition.icon}`
-    nextdayIcon.src = y
-}
-// search
-l.addEventListener("keyup",function () {
-    locationCity(l.value)
-    weather(l.value)
+    if(!data.error){
+    // today
+    let todayDate = new Date()
+    todayName.innerHTML = todayDate.toLocaleDateString("en-US", { weekday: "long" })
+    month.innerHTML = todayDate.getDate() +' '+ todayDate.toLocaleDateString("en-US", { month: "long" })
+    cityName.innerHTML = data.location.name
+    citydegree.innerHTML = data.current.temp_c + "°C"
+    weatherState.setAttribute("src", `http:` + data.current.condition.icon)
+    humidity.innerHTML = data.current.humidity + "%"
+    wind.innerHTML = data.current.wind_kph + "km/h"
+    windDirection.innerHTML = data.current.wind_dir
+    conditionToday.innerHTML = data.current.condition.text
+
+    //next day
+    let forecastData = data.forecast.forecastday
+    for (let i = 0; i < 2; i++) {
+        let nextDate = new Date(forecastData[i+1].date)
+        nextDay[i].innerHTML = nextDate.toLocaleDateString("en-US",{weekday:"long"})
+        maxTemp[i].innerHTML = forecastData[i + 1].day.maxtemp_c + " °C"
+        minTemp[i].innerHTML = forecastData[i + 1].day.mintemp_c + " °C"
+        icon[i].setAttribute("src", `http:` + forecastData[i + 1].day.condition.icon)
+        state[i].innerHTML = forecastData[i + 1].day.condition.text
+    }
+    }
     
+}
+
+// search
+searchByName.addEventListener("keyup", function () {
+    weatherData(searchByName.value)
+
 })
-// time
-let time = date.getHours()
-if (4 < time && time < 17) {
-    weatherState.src = "img/sun.png"
-}
-else {
-    weatherState.src = "img/moon.png"
-}
-// get day
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let day = date.getDay()
-var dayName = days[day];
-todayName.innerHTML = dayName
-if (day == 6) {
-    day = 0
-    tomorrowName.innerHTML = days[day]
-    nextDayName.innerHTML = days[day + 1]
-}
-else if (day == 5) {
-    tomorrowName.innerHTML = days[day + 1]
-    nextDayName.innerHTML = days[0]
-}
-else {
-    tomorrowName.innerHTML = days[day + 1]
-    nextDayName.innerHTML = days[day + 2]
-}
-// get month
-const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-];
-let monthName = date.getMonth()
-let dayNum = date.getDate()
-month.innerHTML = dayNum + ` ` + monthNames[monthName]
+
 
 
 
